417bf33936607fee12c822af7cead27db86ce214
MarkMburu <markmburu685@gmail.com>
2020-09-01 03:54:33 +0300

    bug fixes

--- src/useFetchNews.js
+++ src/useFetchNews.js
@@ -6,7 +6,7 @@ const ACTIONS ={
   GETDATA: 'get-data',
   ERROR: 'error'
 }
-const BASE_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=107794bbfbf144c1aa5113b45bb373b5"
+const BASE_URL = "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&apiKey=107794bbfbf144c1aa5113b45bb373b5"
 const reducer=(state,action)=>{
   switch(action.type){
     case ACTIONS.MAKE_REQUEST:
