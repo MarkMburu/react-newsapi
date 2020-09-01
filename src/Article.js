fdfc2a41012e7e9444bddb0e4f1da4961b10afa0
MarkMburu <markmburu685@gmail.com>
2020-09-01 14:57:31 +0300

    added fonts

--- src/Article.js
+++ src/Article.js
@@ -9,14 +9,14 @@ export default function Article({article}) {
       <div className="d-flex justify-content-between">
       <img className="d-none d-md-block" width="50%" alt="" src={article.urlToImage}/>
         <div>
-          <Card.Title >
+          <Card.Title className="title">
             {article.title} 
   {/* <span className="text-muted font-weight-light"> By {article.author}</span> */}
             </Card.Title>
             <Card.Subtitle className="text-muted mb-2">
             {new Date(article.publishedAt).toLocaleString()}
             </Card.Subtitle>
-            <div style={{wordBreak:'break-all',textOverflow: "ellipsis"}}>
+            <div className="description" style={{wordBreak:'break-all',textOverflow: "ellipsis"}}>
               { article.description }
             </div>
           </div>
